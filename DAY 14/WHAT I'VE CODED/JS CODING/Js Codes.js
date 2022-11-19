73cebef39338747ac701995f8c9e9a3c04706e69
mohamed m. ali <101191058+mohamed20039@users.noreply.github.com>
2022-11-19 09:09:02 -0800

    DAY 14

---
+++ DAY 14/WHAT I'VE CODED/JS CODING/Js Codes.js
@@ -0,0 +1,46 @@
+console.log("Hello World!")
+
+
+console.log("Mohamed Mohamuud")
+
+alert("Iam doing 100 days of coding challenge did you support me?")
+//tip calculator
+
+food=30
+tipPercentage=0.2
+
+tipAmount=food*tipPercentage
+
+console.log("The Amount of tip you gonna pay is ",tipAmount)
+
+// How to get user input in javascript
+//am gonna do it with this javascript tip calculator project
+
+foodPrice=prompt("Enter the amount of money food you ate")
+
+tipPercentage_=0.2
+
+tipAmount_=foodPrice*tipPercentage_
+
+console.log("The amount you of tip gonna pay is ",tipAmount_)
+
+
+//Variable declaring
+
+let b=20
+console.log(b)
+
+var c="Amazing Night"
+
+const hi="It's so amazing when u addict to coding"
+
+
+//Concatination 
+
+var firstName="Mohamed"
+var age=19
+var university="Simad University"
+
+
+console.log("Your name is"+firstName+" And you're "+age+" years old "+university+" Also you go to "+university+" We know everything from you don't try to tell lie 😂😂😂😂😂😂")
+
---
+++ DAY 14/WHAT I'VE LEARNED/README.md
@@ -0,0 +1,11 @@
+Hi guys its say 14 of 100 coding challenge so lemme tell what i've learned today
+
+1.Today i've started learning javascript
+2.It was very basical day 😂😂 i learned the basics of javascript
+2.I've learned how display something in javascript.
+3.Today i also learned how to declare variable in javascript.
+4.What is defference btw Let,Var and Const.
+
+5.How to get javascript user input
+6.Javascript Alert
+
