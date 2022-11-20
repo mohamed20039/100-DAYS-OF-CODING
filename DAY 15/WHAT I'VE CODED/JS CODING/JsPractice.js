ad210e136008175065ceb13e9962e72c8115e6b5
mohamed m. ali <101191058+mohamed20039@users.noreply.github.com>
2022-11-20 11:49:48 -0800

    DAY 15

---
+++ DAY 15/WHAT I'VE CODED/JS CODING/JsPractice.js
@@ -0,0 +1,65 @@
+//number comparison
+20 == 20
+
+20 != 20 // this is gonna be false because 20 and 20 are equal
+
+13>12 // this true 13 is greater than
+
+12<11 //this is also true
+
+12<=11 // this is true because of this =
+
+
+// this is comment
+
+//Some console methods 
+
+console.error("This Error")
+//subject points calculator
+
+sub1=prompt("Enter subject 1");
+
+sub2=prompt("Enter subject 2");
+
+sub3=prompt("Enter subject 3");
+
+total=sub1+sub2+sub3
+
+avg=total/3
+
+console.log("Your average is ",avg)
+
+console.log("We're facing error here ")
+
+//we can solve it to tell the compiler the data type because right it taking as string and that's the default.
+
+sub1=Number(prompt("Enter subject 1"));
+
+sub2=Number(prompt("Enter subject 2"));
+
+sub3=Number(prompt("Enter subject 3"));
+
+total=sub1+sub2+sub3
+
+avg=total/3
+
+console.log("Your average is ",avg)
+
+//now everything is ok
+
+// we are okay right now😂😂😂
+
+//Data Types
+
+//numbers ➡️ 1,2,5,88,100,2000,1000001 ,etc
+
+//string "they have quotation around them" like this "Mohammed" and qaotation can single qoautation or double like this "ALi" 'James'
+
+//arrays []
+
+//objects {}
+
+//boolean true/false
+
+//
+
---
+++ DAY 15/WHAT I'VE LEARNED/README.md
@@ -0,0 +1,8 @@
+Hi guys its say 14 of 100 coding challenge so lemme tell what i've learned today
+
+1.Today i've learned something new javascript
+2.It was very basical day as yesterday😂😂 i learned the basics of javascript
+2.I've learned how display error message in javascript.
+3.Today i also learned Data types.
+4.Error handling in data types
+
