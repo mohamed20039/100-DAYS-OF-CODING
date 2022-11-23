66a458cf9176d5f124c5b6551a8177ed95de9fbc
mohamed m. ali <101191058+mohamed20039@users.noreply.github.com>
2022-11-23 11:14:40 -0800

    DAY 18

---
+++ DAY 18/WHAT I'VE CODED/Youtube Video Downloader/Youtube Video Downloader.py
@@ -0,0 +1,16 @@
+#pip install pytube
+url=input("Enter the youtube link: ")
+my_video=YouTube(url)
+from pytube import YouTube
+#this's part shows the title of the video
+print(f"The title of the video is: {my_video.title}")
+
+#this's part shows the thumbnail of the video
+
+print(f"Here is the thumbnail of the video: {my_video.thumbnail_url}")
+
+#this's the part that let you download the video
+
+my_video=my_video.streams.get_highest_resolution()
+
+print("Dowloading the video")
---
+++ DAY 18/WHAT I'VE LEARNED/README.md
@@ -0,0 +1,8 @@
+Hi guys its say 18 of 100 coding challenge so lemme tell what i've learned today
+
+1.Today i learned python library called pytube
+2.I learned basics of that library and builded projects with it
+3.I learned how to download video from youtube using this library
+5.I learned how to see video's title , views and thumbbaild using this library.
+6.and more
+
