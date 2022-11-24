70e353840433f10cfad87a23b74c26ae24856e0f
mohamed m. ali <101191058+mohamed20039@users.noreply.github.com>
2022-11-24 11:06:33 -0800

    DAY 19

---
+++ DAY 19/WHAT I'VE CODED/JS CODING/index.js
@@ -0,0 +1,45 @@
+// another day of javascript
+
+//Template string
+
+//Guess the crazy the crazy that i've learned today 😂😂😂 it's a little symbol called back ticks i wasn't even what it was what but i learned and javascript is the reason
+
+MyName = 'Mohamed'
+age = 19
+MyUniversity = 'Simad University'
+
+console.log(`Hi my name is ${MyName} iam ${age} years old and i go to ${MyUniversity}`)
+
+// null and undefined data types
+
+// so basically null means that variable is empty and undefined means nobody knows what is inside there😂😂😂😂
+
+let x = null
+let z = undefined
+console.log(x, z)
+
+console.warn("Hey you're someone is hacking you i'm just kidding😂😂😂😂😂")
+
+//How to look for the length of variable
+// and you have to that this function count everything in the qoutation even the spaces
+
+const text = 'Am doing 100 days of coding'
+
+console.log(`The length of this text is ${text.length}`)
+
+// how to make a string lowercase
+//note iam using the above variable called text
+
+console.log(`The lowercase of the latters is here ( ${text.toLowerCase()} )`)
+
+console.log(`The uppercase of this variable is here ( ${text.toUpperCase()} )`)
+
+//substring
+//So basically what does substring do is to tell the number of indexes you wanna display from starting n to n
+
+// and also there is another cool feature about this method and it's you can change the indexes that you choose to a lower case or uppercase
+
+console.log(text.substring(0, 8).toUpperCase())
+
+
+console.log(text.split(''))
---
+++ DAY 19/WHAT I'VE LEARNED/README.md
@@ -0,0 +1,7 @@
+Hi guys its say 19 of 100 coding challenge so lemme tell what i've learned today
+
+1.Today i resumed my javascript course
+2.I learned some basic things like warning display
+3.I learned some new methods and they are Uppercasing ,Lowercasing ,Template strings ,spliting,length and substringing
+
+
